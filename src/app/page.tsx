fffe6ce4bc9cd1dac5def5262ef9ad500e2f2220
Fahmi Dwi Syahputra <itdev.support15@tokiomarine-life.co.id>
2025-05-30 15:37:22 +0700

    WIP: welcome page - add button components - create button sign with goggle - create star on github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardDescription } from "@/components/ui/card";
+import Link from "next/link";
+import { FaGithubAlt, FaGoogle } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -6,17 +9,33 @@ export default function Home() {
       <main>
         <div className="flex flex-col justify-center items-center h-screen">
           <Card className="px-5 py-10 border-foreground/25 max-w-2xl gap-2">
-            <p className="text-3xl font-bold">Symptoms AI</p>
-            <p className="text-2xl text-pretty font-semibold">
+            <div className="flex flex-row justify-between">
+              <p className="text-3xl font-bold">Symptoms AI</p>
+              <a
+                href="https://github.com/dwidev/symponts"
+                target="_blank"
+                className="flex flex-row justify-between"
+              >
+                <Button className="flex flex-row">
+                  <FaGithubAlt /> Star on Github
+                </Button>
+              </a>
+            </div>
+            <p className="text-pretty">
               A modern AI-powered platform for health symptom analysis
             </p>
-            <CardDescription>
+            <CardDescription className="mb-5">
               <p className="text-pretty">
                 Online tool that helps you input symptoms, get instant AI-based
                 suggestions, and make informed health decisions easily and
                 securely.
               </p>
             </CardDescription>
+            <Button asChild className="flex flex-row">
+              <Link href="/welcome">
+                <FaGoogle /> Sign in with Google
+              </Link>
+            </Button>
           </Card>
         </div>
       </main>
