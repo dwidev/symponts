920cb306941adfe7326cff8f6fc1d949b5892cb4
Fahmi Dwi Syahputra <itdev.support15@tokiomarine-life.co.id>
2025-06-26 14:47:42 +0700

    - revert strategry auth from session db to jwt

--- src/lib/auth/auth.config.ts
+++ src/lib/auth/auth.config.ts
@@ -2,9 +2,22 @@ import GitHub from "next-auth/providers/github";
 import { env } from "../env";
 import { NextAuthConfig } from "next-auth";
 
+const cookiePrefixName = "symptoms.ai";
+
 export default {
-  session: { strategy: "database" },
+  session: { strategy: "jwt" },
   secret: env.AUTH_SECRET,
+  cookies: {
+    sessionToken: {
+      name: `${cookiePrefixName}.ses.token`,
+    },
+    csrfToken: {
+      name: `${cookiePrefixName}.csrf.token`,
+    },
+    callbackUrl: {
+      name: `${cookiePrefixName}.callback`,
+    },
+  },
   providers: [
     GitHub({
       clientId: env.AUTH_GITHUB_CLIENT_ID,
--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -6,7 +6,18 @@ import authConfig from "@/lib/auth/auth.config";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(db),
   ...authConfig,
+
   callbacks: {
+    async redirect({ url, baseUrl }) {
+      const dashboard = baseUrl + "/dashboard";
+      if (url.startsWith(baseUrl)) {
+        const { searchParams } = new URL(url);
+        const callbackUrl = searchParams.get("callbackUrl");
+        return callbackUrl || dashboard;
+      }
+
+      return dashboard;
+    },
     async session({ session, user }) {
       if (user) {
         session.user.id = user.id;
