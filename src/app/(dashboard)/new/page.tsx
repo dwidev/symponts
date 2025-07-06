447b290cf5b7020cb96fe53a1b0a19b33a349e1e
Fahmi Dwi S <dwifhmi.developer@gmail.com>
2025-07-06 22:20:34 +0700

    WIP: create new page

--- src/app/(dashboard)/new/page.tsx
+++ src/app/(dashboard)/new/page.tsx
@@ -1,24 +1,30 @@
 import InputPrompt from "@/components/shared/input-promp";
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 export default function NewChatPage() {
   return (
     <div className="size-full flex flex-col items-center">
-      <div className="lg:w-2xl h-full flex flex-col justify-center gap-20 mx-10">
-        <h1 className="text-4xl">Selamat datang di Sysmponts!</h1>
+      <div className="lg:w-2xl h-full flex flex-col justify-center gap-15 mx-10">
+        <div>
+          <h1 className="text-2xl text-gray-600 font-semibold">Haii Fahmi!</h1>
+          <h1 className="text-3xl">Apa keluhan kamu?!</h1>
+        </div>
         <div className="flex flex-col gap-5">
           <div className="flex flex-wrap gap-2">
-            <div className="bg-white px-4 py-2 cursor-pointer shadow-xl w-fit rounded-2xl">
-              <p className="text-sm text-gray-400">Saya sakit kepala</p>
-            </div>
-            <div className="bg-white px-4 py-2 cursor-pointer shadow-xl w-fit rounded-2xl">
-              <p className="text-sm text-gray-400">Saya sakit kepala</p>
-            </div>
-            <div className="bg-white px-4 py-2 cursor-pointer shadow-xl w-fit rounded-2xl">
-              <p className="text-sm text-gray-400">Saya sakit kepala</p>
-            </div>
+            {[0, 1, 2, 3].map((e) => {
+              return (
+                <Button
+                  key={e}
+                  variant="ghost"
+                  className="bg-white px-4 py-2 rounded-full transition hover:scale-105 shadow-2xl"
+                >
+                  <p className="text-sm text-gray-600">Saya sakit kepala</p>
+                </Button>
+              );
+            })}
           </div>
-          <div className="bg-white px-5 py-2 rounded-2xl shadow-xl">
+          <div className="bg-white px-5 py-2 rounded-2xl shadow-2xl">
             <InputPrompt />
           </div>
         </div>
--- src/components/shared/input-promp.tsx
+++ src/components/shared/input-promp.tsx
@@ -5,24 +5,57 @@ import { Textarea } from "../ui/textarea";
 import { Button } from "../ui/button";
 import { FaArrowUp } from "react-icons/fa";
 import { useRouter } from "next/navigation";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Brain, LucideSparkles } from "lucide-react";
 
 export default function InputPrompt() {
   const router = useRouter();
   return (
-    <div className="w-full flex flex-row items-end">
+    <div className="w-full flex flex-col">
       <Textarea
         placeholder="Describe your current condition"
-        className="bg-transparent max-h-50 min-h-30 resize-none border-none shadow-none focus-visible:ring-0 mr-2"
+        className="bg-transparent max-h-50 resize-none border-none shadow-none focus-visible:ring-0 mr-2"
       />
-      <Button
-        size="icon"
-        className="size-7 rounded-full transition hover:scale-105 shadow-xl"
-        onClick={() => {
-          router.push("/chat/asdasd");
-        }}
-      >
-        <FaArrowUp />
-      </Button>
+      <div className="flex flex-row items-center justify-between mb-2 mt-3">
+        <div className="flex flex-row gap-2">
+          <Button variant="outline" className="rounded-full" size="sm">
+            <div className="flex flex-row items-center gap-2">
+              <Brain className="text-red-300" />
+              <p className="text-gray-600 text-[12px]">Gemini</p>
+            </div>
+          </Button>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild className="focus:outline-0">
+              <Button variant="outline" className="rounded-full" size="sm">
+                <div className="flex flex-row items-center gap-2">
+                  <LucideSparkles className="text-yellow-300" />
+                  <span className="text-gray-600 text-[12px]">explore</span>
+                </div>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem>Cek BMI</DropdownMenuItem>
+              <DropdownMenuItem>Cek Kehamilan</DropdownMenuItem>
+              <DropdownMenuItem>Team</DropdownMenuItem>
+              <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        <Button
+          size="icon"
+          className="size-9 rounded-full transition hover:scale-105 shadow-xl"
+          onClick={() => {
+            router.push("/chat/asdasd");
+          }}
+        >
+          <FaArrowUp />
+        </Button>
+      </div>
     </div>
   );
 }
