ab20e7c8c86939f7846ba8a8bdf4f6f3a969e14d
Fahmi Dwi Syahputra <itdev.support15@tokiomarine-life.co.id>
2025-07-07 14:40:22 +0700

    - tidy ui input prompt

--- src/components/shared/input-promp.tsx
+++ src/components/shared/input-promp.tsx
@@ -11,7 +11,8 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
-import { Brain, LucideSparkles } from "lucide-react";
+import { Brain, LucideChevronDown, LucideSparkles } from "lucide-react";
+import { Separator } from "../ui/separator";
 
 export default function InputPrompt() {
   const router = useRouter();
@@ -22,19 +23,14 @@ export default function InputPrompt() {
         className="bg-transparent max-h-50 resize-none border-none shadow-none focus-visible:ring-0 mr-2"
       />
       <div className="flex flex-row items-center justify-between mb-2 mt-3">
-        <div className="flex flex-row gap-2">
-          <Button variant="outline" className="rounded-full" size="sm">
-            <div className="flex flex-row items-center gap-2">
-              <Brain className="text-red-300" />
-              <p className="text-gray-600 text-[12px]">Gemini</p>
-            </div>
-          </Button>
+        <div className="flex flex-row gap-3">
           <DropdownMenu>
             <DropdownMenuTrigger asChild className="focus:outline-0">
               <Button variant="outline" className="rounded-full" size="sm">
                 <div className="flex flex-row items-center gap-2">
                   <LucideSparkles className="text-yellow-300" />
                   <span className="text-gray-600 text-[12px]">explore</span>
+                  <LucideChevronDown />
                 </div>
               </Button>
             </DropdownMenuTrigger>
@@ -46,15 +42,27 @@ export default function InputPrompt() {
             </DropdownMenuContent>
           </DropdownMenu>
         </div>
-        <Button
-          size="icon"
-          className="size-9 rounded-full transition hover:scale-105 shadow-xl"
-          onClick={() => {
-            router.push("/chat/asdasd");
-          }}
-        >
-          <FaArrowUp />
-        </Button>
+        <div className="flex space-x-4">
+          <Button variant="outline" className="rounded-full" size="sm">
+            <div className="flex flex-row items-center gap-2">
+              <Brain className="text-red-300" />
+              <p className="text-gray-600 text-[12px]">Gemini</p>
+              <LucideChevronDown />
+            </div>
+          </Button>
+          <div>
+            <Separator orientation="vertical" />
+          </div>
+          <Button
+            size="icon"
+            className="size-9 rounded-full transition hover:scale-105 shadow-xl"
+            onClick={() => {
+              router.push("/chat/asdasd");
+            }}
+          >
+            <FaArrowUp />
+          </Button>
+        </div>
       </div>
     </div>
   );
